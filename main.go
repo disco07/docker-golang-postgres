05142796e2bcdf933d332f5435edea722ec6ddeb
disco07 <koneenok@outlook.fr>
2022-08-30 06:52:57 +0200

    commit

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "time"
+
+type post struct {
+	ID        int
+	Title     string
+	Slug      string
+	Summary   string
+	Content   string
+	PublishAt time.Time
+}
+
 func main() {
 
 }
