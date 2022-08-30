97ef6102a5c6b75f561e436595a2918d4d19c71a
disco07 <koneenok@outlook.fr>
2022-08-30 06:54:19 +0200

    commit

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import "time"
 
 type post struct {
-	ID        int
-	Title     string
-	Slug      string
-	Summary   string
-	Content   string
-	PublishAt time.Time
+	ID          int
+	Title       string
+	Slug        string
+	Summary     string
+	Content     string
+	PublishedAt time.Time
 }
 
 func main() {
